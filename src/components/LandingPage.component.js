e07f8a30a62f99f89d132c3f6a31737e3e70e086
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-12-09 00:20:57 +0200

    post

--- src/components/LandingPage.component.js
+++ src/components/LandingPage.component.js
@@ -9,14 +9,19 @@ function LoginBox() {
     return(
         <Container className="login-box">
             <Row > {/**noGutters={true} */}
-            <Col className="images-login">
-                <Image src={study_management} fluid="true"  className="study-photo"></Image>
-                <Image src={login_people_books} fluid="true"  className="people-photo"></Image>
-            </Col>
-            <Col>
-                <FormLogin></FormLogin>
-            </Col>
-                {/* <Form_Login/> */}
+                <Col className="images-login">
+                    <Container>
+                        <Row>
+                            <Image src={study_management} fluid="true"  className="study-photo"></Image>
+                        </Row>
+                        <Row>
+                            <Image src={login_people_books} fluid="true"  className="people-photo"></Image>
+                        </Row>
+                    </Container>
+                </Col>
+                <Col>
+                    <FormLogin></FormLogin>
+                </Col>
             </Row>
         </Container>
     );
@@ -25,11 +30,6 @@ function LoginBox() {
 function LandingPage () {
     return(
         <LoginBox></LoginBox>
-        // <div>
-        //     <h1> welcome to the main page</h1>
-        //     <h1><Link to='/'>go to page "/"</Link></h1>
-        //     <h2><Link to='/create'>go to page "/create"</Link></h2>
-        // </div>
     )
 }
 
