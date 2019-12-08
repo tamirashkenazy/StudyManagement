eaebc83b0e8bae3fba13a1f46e80e1a327a6ea8f
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-12-08 17:12:40 +0200

    login-http-get

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from './logo.svg';
 import './styles/App.scss';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route} from 'react-router-dom';
-
+import {Button } from 'semantic-ui-react'
 import CreateUser from './components/create-user.component'
-import LandingPage from './components/LandingPage..component';
+import LandingPage from './components/LandingPage.component';
 
 function App() {
   return (
     <Router>
       <div id="land-page" className="land_page bg">
         <LandingPage />
+        {/* <Button> hey </Button> */}
       </div>
     </Router>
   );
