d208df7a158dffcdbff41bb471c567ebfad794f9
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-12-08 17:12:40 +0200

    login-http-get

--- src/components/loginForm.component.js
+++ src/components/loginForm.component.js
@@ -1,75 +1,84 @@
 
-import React from 'react'
-import {Form , Button} from 'react-bootstrap'
+import React, { Component } from 'react'
+// import {Form , Button} from 'react-bootstrap'
+import {Form , Button, Checkbox, FormField} from 'semantic-ui-react'
+import axios from 'axios'
 import '../styles/login-form.scss'
-function WelcomeHeader() {
-    return (
-        <h3 id="welcome">
-            ברוכים הבאים
-            <h4 id="welcome-to-where">
-                מערכת לניהול שעות החונכות
-            </h4>
-        </h3>
-    )
-}
-function FormLogin () {
-    return (
-        <div className="right-align">
-            <WelcomeHeader/>
-            <Form>
-                <Form.Group controlId="login-username">
-                    <Form.Label>שם משתמש</Form.Label>
-                        <Form.Control type="text" placeholder="ת.ז" />
-                    <Form.Text className="text-muted">
-                        תעודת זהות
-                    </Form.Text>
-                </Form.Group>
 
-                <Form.Group controlId="login-password">
-                    <Form.Label>סיסמה</Form.Label>
-                    <Form.Control type="password" placeholder="סיסמה" />
-                </Form.Group>
-                <Form.Group controlId="forgotPassword">
-                    <div class="form-check" className="check">
-                        <label title="" for="forgotPassword" class="form-check-label">
-                        שכחתי סיסמה
-                        </label>
-                        <input type="checkbox" id="forgotPasswordCheckBox" class="form-check-input" ></input>
-                        
-                    </div>
-                    {/* <Form.Check type="checkbox" label="שכחתי סיסמה" /> */}
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    התחבר
-                </Button>
-            </Form>
-            {/* <Form>
-                <h5 id="comment-user-name">שם המשתמש והסיסמא של האינבר</h5>
-                <Form.Field className="input-field">
-                    <Input icon='user outline' placeholder='שם משתמש' />
-                </Form.Field>
+class FormLogin extends Component  {
+    constructor(props) {
+        super(props)
+        this.state = { 
+            username: '', 
+            password: '', 
+            forgotPassword : false,
+        }
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
-                <Form.Field className="input-field">
-                    <Input icon='lock' placeholder='סיסמא' />
-                </Form.Field>
+    handleChange = (e, { name, value }) => {
+        this.setState({ [name]: value })
+    }
 
-                <Form.Field id="buttons-login-id" className="buttons-login">
-                    <button id="login-btn" className="btn" ><div>כניסה</div></button>
-                    <Modal trigger={Signup_Button()} centered={true}>
-                        <Modal.Content>
-                            <Modal.Description>
-                                <Header>
-                                    <h1>hey</h1>
-                                </Header>
-                            </Modal.Description>
-                        </Modal.Content>
-                    </Modal>
+    // handleSubmit = (e) => {
+    //     const { username, password } = this.state
+    //     console.log(username, password)
+    // }
+    onSubmit(e) {
+        e.preventDefault();
+        const {username} = this.state
+        console.log(username);
+        axios.get('http://localhost:5000/users/'+ username)
+        .then(res => console.log("data: " + res.data.map(user => user.id_number)));
+    }
+    WelcomeHeader = () => {
+        return (
+            <div>
+                <h3 id="welcome">ברוכים הבאים</h3>    
+                <h4 id="welcome-to-where">מערכת לניהול שעות החונכות</h4>
+                <br></br>
+            </div>
+        )
+    }
+
+    render() {
+        const { username, password } = this.state
+        return (
+            <div className="right-align">
+                <this.WelcomeHeader/>
+                <Form onSubmit={this.onSubmit}>
+                    <FormField>
+                        <label>שם משתמש</label>
+                        <Form.Input
+                            placeholder='ת.ז'
+                            name='username'
+                            value={username}
+                            onChange={this.handleChange}
+                        />
+                    </FormField>
                     
-                </Form.Field>
-            </Form> */}
-        </div>
-
-    )
+                    <FormField>
+                        <label>סיסמה</label>
+                        <Form.Input
+                            type="password"
+                            placeholder='סיסמה'
+                            name='password'
+                            value={password}
+                            onChange={this.handleChange}
+                        />
+                    </FormField>
+                    
+                    <Form.Field
+                            control={Checkbox}
+                            label='שכחתי סיסמה'
+                        />
+                    <Form.Field control={Button}>התחבר</Form.Field>
+                    {/* <Button onClick={this.handlePrint}>print state</Button>  */}
+                </Form>
+                
+            </div>
+        )
+    }
 }
 
 export default FormLogin;
