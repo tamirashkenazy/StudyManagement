fc97e16fde59ecd86e399b92ae34b9fcbd87ba8e
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-12-15 16:42:47 +0200

    changed nammes

--- client/src/components/signup/signupForm.component.js
+++ client/src/components/signup/signupForm.component.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Container} from 'react-bootstrap'
 import { Form,  Label, Dropdown, Button, Radio, Checkbox, Icon} from 'semantic-ui-react';
 import axios from 'axios'
-import {Link , Redirect } from 'react-router-dom'
+import {Link , useHistory } from 'react-router-dom'
 import '../../styles/signup-form.scss'
 import '../../styles/general.scss'
 import {validateForm, check_and_assign_errors, error_default_messages}  from './validationFields'
 
-class SignupForm extends Component {
-    constructor(props) {
-        super(props)
-        this.state = { 
+function SignupForm() {
+    let history = useHistory()
+
+    const useSignUpForm = (callback) => {
+        const [userState, setUserState] = useState({
             first_name: '',
             last_name: '',
             id_number: '', 
@@ -21,98 +22,85 @@ class SignupForm extends Component {
             gender : '',
             isStudent : false,
             isTeacher : false,
-            errors : {
-                first_name: error_default_messages.first_name,
-                last_name: error_default_messages.last_name,
-                id_number:error_default_messages.id_number,
-                tel_number:  error_default_messages.tel_number,
-                password:  error_default_messages.password,
-                year :error_default_messages.year,
-                email :  error_default_messages.email,
-                gender : error_default_messages.gender,
-                role : error_default_messages.role,
-            },
-            isLoading : true,
-            redirect : false
+        })
+        const [validForm, setValidForm] = useState(false)
+        const [errors, setErrors] = useState({
+            first_name_error: error_default_messages.first_name,
+            last_name_error: error_default_messages.last_name,
+            id_number_error : error_default_messages.id_number,
+            tel_number_error:  error_default_messages.tel_number,
+            password_error:  error_default_messages.password,
+            year_error :error_default_messages.year,
+            email_error :  error_default_messages.email,
+            gender_error : error_default_messages.gender,
+            role_error : error_default_messages.role,
+    })
+        const handleSubmit = (event) => {
+            if (event) {
+                event.preventDefault();
+                callback()
+            }
+            
         }
-        
-        this.onSignUp = this.onSignUp.bind(this);
-    }
-    
-    
-    handleChange = (event, { name, value }) => {
-        let temp_errors = this.state.errors
-        temp_errors = check_and_assign_errors(name, value, temp_errors)
-        let valid = validateForm(temp_errors)
-        this.setState(prevState => ({ valid_form :  (valid && (prevState.isStudent || prevState.isTeacher)), [name]: value, errors : temp_errors }))
+        const handleInputChange = (event, {name, value, checked, type}) => {
+            //If you want to access the event properties in an asynchronous way, 
+            //you should call event.persist() on the event, which will remove the synthetic event from the pool  
+            //and allow references to the event to be retained by user code.
+            const local_value = (type === 'checkbox') ? checked : value
+            event.persist();
+            let valid_form = false
+            if (type !== 'checkbox') {
+                let temp_errors = check_and_assign_errors(name, local_value, errors)
+                valid_form = validateForm(temp_errors)
+                setValidForm(valid_form && (userState.isStudent || userState.isTeacher))
+                setErrors(temp_errors)
+            } else {
+                if (checked || (name==='isStudent' && userState.isTeacher || name==='isTeacher' && userState.isStudent)) {
+                    valid_form = true
+                }
+                let role_error= null
+                if (!valid_form) {
+                     role_error = error_default_messages.role
+                }
+                setErrors(oldErrors => ({...oldErrors, role_error : role_error}))
+                //the validForm is validate the fields without relation to checkbox
+                setValidForm((validateForm(errors) && (valid_form)))
+
+            }
+            setUserState(inputs => ({...inputs, [name] : local_value}));
+        }
+        return { handleSubmit, handleInputChange, userState, validForm, errors };
     }
 
-    handleCheckbox = (e, { name, checked }) => {
-        const { isTeacher, isStudent, errors} = this.state
-        let role_valid = false;
-        switch(name){
-            case 'isStudent':
-                if(checked || isTeacher) {
-                    role_valid = true
-                    errors.role = null
-                }
-            break;
-            case 'isTeacher':
-                if(checked || isStudent) {
-                    role_valid = true
-                    errors.role = null
-                }
-            break;
-            default:
-            break;
+    const httpPostRequestToAddUser = () => {
+        const {id_number, password, email, first_name, last_name, tel_number, gender, isTeacher, isStudent, year } = userState
+        const user_to_add = {  
+            _id : id_number,
+            password : password,
+            email: email,
+            first_name : first_name, 
+            last_name: last_name, 
+            tel_number : tel_number,
+            gender : gender,
+            isTeacher : isTeacher,
+            isStudent: isStudent,
+            isAdmin : false,
+            study_year: year,
         }
-        if(!role_valid){
-            errors.role =error_default_messages.role
-        }
-        this.setState(prevState => ({
-                [name] : !prevState[name],
-                valid_form :  (validateForm(errors) && (role_valid)),
-                errors : errors,
-            })
-        )
-    }
-    
-    onSignUp = (e) => {
-        e.preventDefault();
-        const { first_name, last_name, id_number, tel_number, password, year, email, gender, isStudent, isTeacher} = this.state
-        const new_user = {  
-                        _id : id_number,
-                        password : password,
-                        email: email,
-                        first_name : first_name, 
-                        last_name: last_name, 
-                        tel_number : tel_number,
-                        gender : gender,
-                        isTeacher : isTeacher,
-                        isStudent: isStudent,
-                        isAdmin : false,
-                        study_year: year,
-                    }
-   
-        console.log(new_user);
-        axios.post('http://localhost:5000/users/add', new_user)
-        .then((response)=>{
-            console.log("success: " + (response.data.success));
-            console.log("message: " + (response.data.message));
-            if (response.data.success) {
-                this.setState({
-                    isLoading : false,
-                    redirect : true
-                });
-            } else {
-                this.setState({
-                    isLoading : false,
-                });
-           }
+        console.log("the user is: " + user_to_add);
+        axios.post('http://localhost:5000/users/add', user_to_add)
+        .then((response)=> {
+                    if (response.data.success) {
+                        history.push('/')
+                    } else {
+                        alert(response.data.message)
+                   }
         })
-    }
-    email_field = () => {
-        const { email } = this.state
+    }  
+    const {userState, validForm, handleInputChange, handleSubmit} = useSignUpForm(httpPostRequestToAddUser);
+
+    const email_field = () => {
+        const { email } = userState
         return (
             <Form.Field required>
                 <label>אימייל</label>
@@ -120,14 +108,14 @@ class SignupForm extends Component {
                     name='email'
                     value={email}
                     placeholder='@e-mail'
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </Form.Field>
         )
     }
     
-    password_field = () => {
-        const { password } = this.state
+    const password_field = () => {
+        const { password } = userState
         return (
             <Form.Field required>
                 <label>סיסמה</label>
@@ -136,13 +124,13 @@ class SignupForm extends Component {
                     placeholder='סיסמה'
                     name='password'
                     value={password}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </Form.Field>
         )
     }
-    id_number_field = () => {
-        const { id_number } = this.state
+    const id_number_field = () => {
+        const { id_number } = userState
         return (
             <Form.Field required>
                 <label>ת.ז</label>
@@ -150,14 +138,14 @@ class SignupForm extends Component {
                     placeholder='ת.ז - שם משתמש'
                     name='id_number'
                     value={id_number}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
                 <Label size="tiny" pointing>שם המשתמש</Label>
             </Form.Field>
         )
     }
-    tel_number_field = () => {
-        const { tel_number } = this.state
+    const tel_number_field = () => {
+        const { tel_number } = userState
         return (
             <Form.Field required>
                 <label>מס' טלפון</label>
@@ -165,15 +153,15 @@ class SignupForm extends Component {
                     placeholder='05xxxxxxxx'
                     name='tel_number'
                     value={tel_number}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </Form.Field>
         )
         
     }
 
-    last_name_field = () => {
-        const { last_name } = this.state
+    const last_name_field = () => {
+        const { last_name } = userState
         return (
             <Form.Field required>
                 <label>שם משפחה</label>
@@ -181,14 +169,14 @@ class SignupForm extends Component {
                     placeholder='שם משפחה'
                     name='last_name'
                     value={last_name}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </Form.Field>
         )
         
     }
-    first_name_field = () => {
-        const { first_name } = this.state
+    const first_name_field = () => {
+        const { first_name } = userState
         return (
             <Form.Field required>
                 <label>שם פרטי</label>
@@ -196,13 +184,13 @@ class SignupForm extends Component {
                     placeholder='שם פרטי'
                     name='first_name'
                     value={first_name}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </Form.Field>
         )
     }
-    genders_field = () => {
-        const {gender} = this.state
+    const genders_field = () => {
+        const {gender} = userState
         return (
             <>
                 <Form.Field 
@@ -211,7 +199,7 @@ class SignupForm extends Component {
                     label='נקבה'
                     value='female'
                     checked={gender === 'female'}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
                 <Form.Field              
                     name="gender"
@@ -219,13 +207,13 @@ class SignupForm extends Component {
                     label='זכר'
                     value='male'
                     checked={gender === 'male'}
-                    onChange={this.handleChange}
+                    onChange={handleInputChange}
                 />
             </>
         )
     }
-    roles_field = () => {
-        const {isStudent, isTeacher} = this.state
+    const roles_field = () => {
+        const {isStudent, isTeacher} = userState
         return (
             <>
                 <Form.Field 
@@ -233,21 +221,21 @@ class SignupForm extends Component {
                     control={Checkbox}
                     label='תלמיד'
                     checked={isStudent}
-                    onChange={this.handleCheckbox}
+                    onChange={handleInputChange}
                 />
                 <Form.Field            
                     name='isTeacher'
                     control={Checkbox}
                     label='מורה'
                     checked={isTeacher}
-                    onChange={this.handleCheckbox}
+                    onChange={handleInputChange}
                 />
             </>
         )
 
     }
 
-    study_year_field = () => {
+    const study_year_field = () => {
         const options = [
             { key: 'a', text: 'שנה א', value: 'year-a' },
             { key: 'b', text: 'שנה ב', value: 'year-b' },
@@ -255,7 +243,7 @@ class SignupForm extends Component {
             { key: 'd', text: 'שנה ד', value: 'year-d' },
             { key: 'e', text: 'שנה ה', value: 'year-e' },
         ]
-        const {year} = this.state
+        const {year} = userState
         return (
             <Form.Field required >
                 <label>שנת לימודים</label>
@@ -265,41 +253,35 @@ class SignupForm extends Component {
                     clearable 
                     options={options} 
                     selection 
-                    onChange={this.handleChange} 
+                    onChange={handleInputChange} 
                 />
             </Form.Field>
         )
     }
-    render() {
-        const {valid_form, redirect} = this.state
-        if (redirect) {
-            return <Redirect to="/"></Redirect>
-        }
-        return (
-            <Form style={{margin:"2%"}}>
-                <Form.Group widths='equal'>
-                    {this.email_field()}
-                    {this.password_field()}
-                    {this.id_number_field()}
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    {this.tel_number_field()}
-                    {this.last_name_field()}
-                    {this.first_name_field()}
-                </Form.Group>
-                <Form.Group widths='equal'>
-                    {this.genders_field()}
-                    {this.roles_field()}
-                    {this.study_year_field()}
-                </Form.Group>
-                <Form.Group>
-                    <Form.Field>
-                        <Button onClick={this.onSignUp} primary disabled={!valid_form}>הירשם</Button>
-                    </Form.Field>
-                </Form.Group>
-            </Form>
-        )
-    }
+    return (
+        <Form style={{margin:"2%"}}>
+            <Form.Group widths='equal'>
+                {email_field()}
+                {password_field()}
+                {id_number_field()}
+            </Form.Group>
+            <Form.Group widths='equal'>
+                {tel_number_field()}
+                {last_name_field()}
+                {first_name_field()}
+            </Form.Group>
+            <Form.Group widths='equal'>
+                {genders_field()}
+                {roles_field()}
+                {study_year_field()}
+            </Form.Group>
+            <Form.Group>
+                <Form.Field>
+                    <Button onClick={handleSubmit} primary disabled={!validForm}>הירשם</Button>
+                </Form.Field>
+            </Form.Group>
+        </Form>
+    )
 }
 
 export default function signupConatainer () {
