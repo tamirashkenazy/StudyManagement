eaabf453ca61ea202dd8f2cd7453ff410db89cd5
Tamir Ashkenazy <tamir.ashkenazy@live.biu.ac.il>
2019-12-15 16:42:47 +0200

    changed nammes

--- client/src/components/loginForm.component.js
+++ client/src/components/loginForm.component.js
@@ -1,58 +1,50 @@
 
-import React, { Component } from 'react'
-import {Link, Redirect} from 'react-router-dom'
+import React, { useState } from 'react'
+import {Link , useHistory} from 'react-router-dom'
 import {Form , Button, Checkbox} from 'semantic-ui-react'
 import axios from 'axios'
 import '../styles/login-form.scss'
 import '../styles/general.scss'
-
-class FormLogin extends Component  {
-    _isMounted = false;
-    constructor(props) {
-        super(props)
-        this.state = { 
-            username: '', 
-            password: '', 
-            forgotPassword : false,
-            redirect : false,
-        }
-        this.onSignIn = this.onSignIn.bind(this);
-    }
-
-    handleChange = (e, { name, value }) => {
-        this.setState({ [name]: value })
-    }
-    
-    onSignIn(e) {
-        e.preventDefault();
-        const {username, password} = this.state
+// form creatiom = https://medium.com/@geeky_writer_/using-react-hooks-to-create-awesome-forms-6f846a4ce57
+function FormLogin ()  {
+    let history = useHistory()
+    const httpPostRequestToGetUser = () => {
         const login_user = {  
-            _id : username,
-            password : password,
+            _id : inputs.username,
+            password : inputs.password,
         }
+        console.log("the user is: " + inputs.username + " " + inputs.password);
         axios.post('http://localhost:5000/sign_in/', login_user).then((response)=> {
-            console.log("success: " + (response.data.success));
-            console.log("message: " + (response.data.message));
-            if (response.data.success) {
-                this.setState({
-                    isLoading : false,
-                    redirect : true,
-                })
-            } else {
-                console.log(response.data.message);
-                console.log("not succ " + response.data.message);
-           }
-        }).then(()=>{
-            this.setState({
-                isLoading : false,
-            });
+                    if (response.data.success) {
+                        history.push({
+                            pathname: '/main',
+                            state: { _id : inputs.username }
+                          })
+                    } else {
+                        alert(response.data.message)
+                   }
         })
     }
-    componentWillUnmount() {
-        this._isMounted = false;
+
+    const useSignInForm = (callback) => {
+        const [inputs, setInputs] = useState({username : '', password : ''})
+        const handleSubmit = (event) => {
+            if (event) {
+                event.preventDefault();
+                callback()
+            }
+            
+        }
+        const handleInputChange = (event, {name, value}) => {
+            event.persist();
+            setInputs(inputs => ({...inputs, [name] : value}));
+        }
+        return { handleSubmit, handleInputChange,  inputs};
     }
 
-    WelcomeHeader = () => {
+    const {inputs, handleInputChange, handleSubmit} = useSignInForm(httpPostRequestToGetUser);
+
+    const WelcomeHeader = () => {
         return (
             <div>
                 <h3 id="welcome">ברוכים הבאים</h3>    
@@ -62,54 +54,49 @@ class FormLogin extends Component  {
         )
     }
 
-    render() {
-        const { username, password, redirect } = this.state
-        if (redirect) {
-            return (
-                <Redirect to={{pathname:'/main', state:{first_name:username} }}></Redirect>
-            )
-        }
-        return (
-            <div className="right-align">
-                <this.WelcomeHeader/>
 
-                <Form className="login-form">
-                    <Form.Field>
-                        <label>שם משתמש</label>
-                        <Form.Input
-                            placeholder='ת.ז'
-                            name='username'
-                            value={username}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Field>
-                    
-                    <Form.Field>
-                        <label>סיסמה</label>
-                        <Form.Input
-                            type="password"
-                            placeholder='סיסמה'
-                            name='password'
-                            value={password}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Field>
-                    
-                    <Form.Field
-                            control={Checkbox}
-                            label='שכחתי סיסמה'
-                        />
 
-                    <Form.Field>
-                        <Link to="/signup">
-                            <Button color="teal">להרשמה</Button>
-                        </Link>
-                        <Button onClick={this.onSignIn} primary>התחבר</Button>
-                    </Form.Field>
-                </Form>
-            </div>
-        )
-    }
+    return (
+        <div className="right-align">
+            <WelcomeHeader/>
+
+            <Form className="login-form">
+                <Form.Field>
+                    <label>שם משתמש</label>
+                    <Form.Input
+                        placeholder='ת.ז'
+                        name='username'
+                        value={inputs.username} // || ''
+                        onChange={handleInputChange}
+                    />
+                </Form.Field>
+                
+                <Form.Field>
+                    <label>סיסמה</label>
+                    <Form.Input
+                        type="password"
+                        placeholder='סיסמה'
+                        name='password'
+                        value={inputs.password} // || ''
+                        onChange={handleInputChange}
+                    />
+                </Form.Field>
+                
+                <Form.Field
+                        control={Checkbox}
+                        label='שכחתי סיסמה'
+                    />
+
+                <Form.Field>
+                    <Link to="/signup">
+                        <Button color="teal">להרשמה</Button>
+                    </Link>
+                    <Button onClick={handleSubmit} primary>התחבר</Button>
+                </Form.Field>
+            </Form>
+        </div>
+    )
+    
 }
 
 export default FormLogin;
