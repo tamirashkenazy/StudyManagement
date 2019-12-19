58e47ba61541fb9b7f7d221adea5f72e8351e105
michaelreu <michael.reubinof@live.biu.ac.il>
2019-12-19 09:10:15 -0800

    user backend fixed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt')
-
 const Schema = mongoose.Schema;
+
+const genders = 'male female'.split(' ')
+onlyNumbers = /^[0-9\b]+$/;
+validEmailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i
+onlyEnglishAndHebrew = /^[A-Za-z\u0590-\u05fe]+$/i
+
 const UserSchema = new Schema({
-    _id :  {type : String, default : '', required : true},
-    password : {type : String, required : true},
-    email : { type: String, required : true },
-    first_name : {type : String,  required : true },
-    last_name : {type : String, required : true },
-    tel_number : { type : String, required : true },
-    gender : { type : String , required : true },
+    _id :  {type : String, default : '', unique: true, required : true, match: onlyNumbers, minlength: 9, maxlength: 9},
+    password : {type : String, required : true, minlength: 4},
+    email : { type: String, required : true, unique: true, match: validEmailRegex},
+    first_name : {type : String,  required : true, match: onlyEnglishAndHebrew},
+    last_name : {type : String, required : true, match: onlyEnglishAndHebrew},
+    tel_number : { type : String, required : true, match:  onlyNumbers, minlength: 10, maxlength: 10},
+    gender : { type : String , required : true, enum: genders},
     isTeacher : { type : Boolean, required : true},
     isStudent : { type : Boolean, required : true},
     isAdmin: { type : Boolean, required : true },
@@ -35,12 +40,18 @@ doc.integerOnly; // 3
 doc.i; // 3
  */
 
+
+
 UserSchema.methods.generateHash = function(password) {
     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 };
 
 UserSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.methods.checkPassword = function(password) {
+  return password.length >= 4;
 };
 
 module.exports = mongoose.model('User', UserSchema);
