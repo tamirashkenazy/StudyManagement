bb510523ea1498bc0fc255b9e601575690203065
michaelreu <michael.reubinof@live.biu.ac.il>
2019-12-21 08:32:14 -0800

    teacher requests ready

--- backend/models/teacherRequest.model.js
+++ backend/models/teacherRequest.model.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose");
 
+let Course = require('../models/course.model').schema;
+const state = 'waiting approved rejected'.split(' ')
+const onlyNumbers = /^[0-9\b]+$/;
+
 const Schema = mongoose.Schema;
 const TeacherRequestsSchema = new Schema({
-   // course : 
-   // status : 
+   teacherId :  {type : String, default : '', required : true, match: onlyNumbers, minlength: 9, maxlength: 9},
+   courseNumber : { type : String, required : true, match: onlyNumbers },
+   status : { type : String , required : true, enum: state},
 }, { timestamps:true }, );
 
 module.exports = mongoose.model('TeacherRequests', TeacherRequestsSchema);
