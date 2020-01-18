74463ddec3ab26bee8b4d0df13f807b060d26318
7105henl <7105henl@gmail.com>
2020-01-18 16:23:43 +0200

    new branch

--- client/src/components/main/student/courses_table.component.js
+++ client/src/components/main/student/courses_table.component.js
@@ -50,30 +50,32 @@ export default function CoursesTable(user_id) {
     const classes = useStyles();
     return (
         !loading && table_rows && 
-        <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label="simple table">
-                <TableHead className={classes.tableHead}>
-                    <TableRow>
-                        <TableCell align="right">תאריך</TableCell>
-                        <TableCell align="right">מזהה קורס</TableCell>
-                        <TableCell align="right">מספר שעות</TableCell>
-                        <TableCell align="right">סטטוס</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                {table_rows.map(row => {
-                    let date = dateFormat(row.date, "dd/mm/yyyy") 
-                    return (
-                        <TableRow key={row.key}>
-                            <TableCell align="right">{date}</TableCell>
-                            <TableCell component="th" scope="row" align="right">{row.course_id}</TableCell>
-                            <TableCell align="right">{row.hours}</TableCell>
-                            <TableCell align="right" style={{color:english_to_hebrew_status[row.status].color}}>{english_to_hebrew_status[row.status].text}</TableCell>
-                        </TableRow>
-                    )
-                })}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <GenericTable table_data={{data:table_rows, title:"קורסים"}}/>
+
+        // <TableContainer component={Paper}>
+        //     <Table className={classes.table} aria-label="simple table">
+        //         <TableHead className={classes.tableHead}>
+        //             <TableRow>
+        //                 <TableCell align="right">תאריך</TableCell>
+        //                 <TableCell align="right">מזהה קורס</TableCell>
+        //                 <TableCell align="right">מספר שעות</TableCell>
+        //                 <TableCell align="right">סטטוס</TableCell>
+        //             </TableRow>
+        //         </TableHead>
+        //         <TableBody>
+        //         {table_rows.map(row => {
+        //             let date = dateFormat(row.date, "dd/mm/yyyy") 
+        //             return (
+        //                 <TableRow key={row.key}>
+        //                     <TableCell align="right">{date}</TableCell>
+        //                     <TableCell component="th" scope="row" align="right">{row.course_id}</TableCell>
+        //                     <TableCell align="right">{row.hours}</TableCell>
+        //                     <TableCell align="right" style={{color:english_to_hebrew_status[row.status].color}}>{english_to_hebrew_status[row.status].text}</TableCell>
+        //                 </TableRow>
+        //             )
+        //         })}
+        //         </TableBody>
+        //     </Table>
+        // </TableContainer>
     )
 }
