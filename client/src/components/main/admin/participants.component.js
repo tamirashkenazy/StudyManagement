cd56116d91304fd50a46e9aab307c0d26e3bf95c
Tamir Ashkenazy <tamir.ash4@gmail.com>
2020-01-21 20:30:58 +0200

    work on admin

--- client/src/components/main/admin/participants.component.js
+++ client/src/components/main/admin/participants.component.js
@@ -9,24 +9,54 @@ import GenericTable from '../utils/generic_table.component';
 
 
 const make_participants = (arr_of_users, args) => {
+    if (!args) {
+        return null
+    }
     const users_by_roles = {teachers : [], students : []}
-    const {setCardOpen, setUserID} = args
+    const {setCardOpen, setUserID, teachers, students} = args
+    console.log('teachers, students : ', teachers, students);
     if (arr_of_users && arr_of_users!==undefined && arr_of_users.length>0){
         arr_of_users.forEach(user => {
+            const id = user._id
+            let courses;
             if (user.isTeacher) {
+                courses = []
+                let teacher = teachers.filter(teacher=> teacher._id === id)
+                if (teacher && teacher.length === 1) {
+                    teacher = teacher[0]
+                    if (teacher.teaching_courses && teacher.teaching_courses.length > 0 ) {
+                        teacher.teaching_courses.forEach(course=>courses.push(course.course_name))
+                        console.log("in the if: ", courses);
+
+                    }
+                } else {
+
+                }
                 users_by_roles.teachers.push({
                     "" : <IconButton onClick={()=>{setCardOpen(true); setUserID(user._id)}}><AccountCircleOutlinedIcon/></IconButton>,
-                    "ת.ז" : user._id,
+                    "ת.ז" : id,
                     "שם פרטי" : user.first_name,
                     "שם משפחה" : user.last_name,
+                    "קורסים" :  courses.join(", ")
                 })
             }
             if (user.isStudent) {
+                courses = []
+                let student = students.filter(student=> student._id === id)
+                if (student && student.length === 1) {
+                    student = student[0]
+                    if (student.requests && student.requests.length > 0 ) {
+                        student.requests.filter(req=>req.status === "approved").forEach(course=>courses.push(course.course_name))
+                    }
+                } else {
+
+                }
                 users_by_roles.students.push({
                     "" : <IconButton onClick={()=>{setCardOpen(true); setUserID(user._id)}}><AccountCircleOutlinedIcon/></IconButton>,
-                    "ת.ז" : user._id,
+                    "ת.ז" : id,
                     "שם פרטי" : user.first_name,
                     "שם משפחה" : user.last_name,
+                    "קורסים" :  courses.join(", ")
                 })
             }
         })
@@ -37,14 +67,22 @@ const make_participants = (arr_of_users, args) => {
 export default function Participants(){
     const [isCardOpen, setCardOpen] = useState(false)
     const [user_id, setUserID] = useState(null)
-    let args = {setCardOpen, setUserID}
+    const [teachers, teachers_loading] = useAsyncHook(`teachers`, null);
+    const [students, students_loading] = useAsyncHook(`students`, null);
+    let args = {setCardOpen, setUserID, teachers, students};
+
+    //this part happens before the  useAsyncHook(`teachers`, null); and  useAsyncHook(`students`, null); - this is the problem
     const [users, loading] = useAsyncHook(`users`, make_participants, args);
-    const panes = [
-    { menuItem: 'מורים', render: () => <Tab.Pane>{<GenericTable table_data={{data:users.teachers, title:null}} />}</Tab.Pane> },
-    { menuItem: 'תלמידים', render: () => <Tab.Pane>{<GenericTable table_data={{data:users.students, title:null}} />}</Tab.Pane> },
-    ]
+    let panes=null
+    if (users) {
+        panes = [
+            { menuItem: 'מורים', render: () => <Tab.Pane>{<GenericTable table_data={{data:users.teachers, title:null}} />}</Tab.Pane> },
+            { menuItem: 'תלמידים', render: () => <Tab.Pane>{<GenericTable table_data={{data:users.students, title:null}} />}</Tab.Pane> },
+            ]
+    }
+
     return (
-        !loading &&  <>
+        !loading && !teachers_loading && !students_loading &&  <>
          <Tab panes={panes}/>
         {Dialog_generator(isCardOpen, ()=>setCardOpen(false), "כרטיס סטודנט" ,{}, ()=><UserCard user_id={user_id}></UserCard>)}
         </>
