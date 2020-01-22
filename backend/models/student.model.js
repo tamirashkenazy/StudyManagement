a7689bc06b8dd679bd2a10f68a6a0147a4557230
michaelreu <michael.reubinof@live.biu.ac.il>
2020-01-22 12:23:51 -0800

    student backend

--- backend/models/student.model.js
+++ backend/models/student.model.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
 
-const status = 'waiting should_pay approved declined '.split(' ')
-const onlyEnglishAndHebrew = /^[ -A-Za-z\u0590-\u05fe]+$/i
+const status = 'waiting payed approved'.split(' ')
 const onlyNumbers = /^[-0-9\b]+$/;
 
 const Schema = mongoose.Schema;
 const StudentSchema = new Schema({
-    _id : {type : String, required : true, match: onlyNumbers, minlength: 9, maxlength: 9},
-    group: {type : String, default : 'normal', required : true},
-    requests : {type : [{course_id : {type : String,  required : true, match: onlyNumbers },
-                 course_name : {type : String,  required : true, match: onlyEnglishAndHebrew},
-                 number_of_hours : {type : String,  required : true, match: onlyNumbers},
+    _id :  {type : String, default : '', required : true, match: onlyNumbers, minlength: 9, maxlength: 9},
+    group : {name: {type : String, default : ''},
+             aproved_hours : {type: String, match: onlyNumbers, default : '4'}},   
+    requests : [{course_id : {type : String,  required : true, match: onlyNumbers },
+                 course_name : { type : String, required : true, minlength: 2},
                  updated_at : {type : Date,  required : true, default: Date.now()},
-                 status : { type : String, required : true, enum: status, default: 'waiting'}}], default : []}
+                 number_of_hours : {type : String,  required : true, match: onlyNumbers},
+                 status : { type : String, required : true, enum: status, default: 'waiting'}}],
+    courses : [{course_id : {type : String,  required : true, match: onlyNumbers },
+                course_name : { type : String, required : true, minlength: 2 },
+                approved_hours : {type : String,  required : true, match: onlyNumbers},
+                wating_hours : {type : String,  required : true, match: onlyNumbers},
+                hours_already_done : {type : String,  required : true, match: onlyNumbers, default: '0'}}] 
 }, { timestamps:true }, );
 
-// defult status
-// only approved courses
-// messege of errors in hebrew
-// all the return will be in same format 
-// all the aviable teacher by status - lessons schema
-// equal === instead of ==
-// remove console.log 
 
 module.exports = mongoose.model('Student', StudentSchema);
--- backend/routes/students.js
+++ backend/routes/students.js
@@ -4,16 +4,16 @@ let Student = require('../models/student.model');
 // all students info
 router.route('/').get((req, res) => {
     Student.find()
-    .then(student => res.send({success : true, message: student}))
+    .then(students => res.send({success : true, message: students}))
     .catch(err => res.status(400).json("Error: " + err));
 });
 
 // the /:id is like a variable
-router.route('/:id').get((req,res) => {
+router.route('/byID/:id').get((req,res) => {
     Student.findById((req.params.id), (err,student) => {
         if(err) {
             return res.send({success : false, message:"Error: " + err})
-        } else if (student && student.length > 0) {
+        } else if (student) {
             return res.send({success : true, message: student})
         } else {
             return res.send({success : false, message:"!הסטודנט אינו קיים במערכת" })
@@ -22,8 +22,8 @@ router.route('/:id').get((req,res) => {
 })
 
 
-// list of student request by student id
-router.route('/:id/request').get((req,res) => {
+// list of student's requests by student id
+router.route('/:id/requests').get((req,res) => {
     Student.findById((req.params.id), (err,student) => {
         if(err) {
             return res.send({success : false, message:"Error: " + err})
@@ -35,19 +35,121 @@ router.route('/:id/request').get((req,res) => {
     })
 })
 
-// add course to student requests list
+//  student request by student id and course_id
+router.route('/:id/requests/byCourseID').post((req,res) => {
+    Student.findById((req.params.id), (err,student) => {
+        if(err) {
+            return res.send({success : false, message:"Error: " + err})
+        } else if (!student || student.length===0) {
+            return res.send({success : false, message:"!הסטודנט אינו קיים במערכת" })
+        } else {
+            student.requests = student.requests.filter(request => request.course_id === req.body.course_id)
+            return res.send({success : true, message: student.requests})
+        }
+    })
+})
+
+
+//  student request by student id and status
+router.route('/:id/requests/byStatus').post((req,res) => {
+    Student.findById((req.params.id), (err,student) => {
+        if(err) {
+            return res.send({success : false, message:"Error: " + err})
+        } else if (!student || student.length===0) {
+            return res.send({success : false, message:"!הסטודנט אינו קיים במערכת" })
+        } else {
+            student.requests = student.requests.filter(request => request.status === req.body.status)
+            return res.send({success : true, message: student.requests})
+        }
+    })
+})
+
+
+// all students with the same group
+router.route('/group').post((req, res) => {
+    Student.find()
+    .then(students => {
+        students = students.filter(student => student.group.name === req.body.group.name)
+        res.send({success : true, message: students})
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+// all requests with same status
+router.route('/status/:status').get((req, res) => {
+    Student.find()
+    .then(students => {
+        requests = []
+        students.forEach(student => requests.push(student.requests.filter(request => request.status === req.params.status)));
+        res.send({success : true, message: requests})
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+// all requests with same course_id
+router.route('/courseID/:courseID').get((req, res) => {
+    Student.find()
+    .then(students => {
+        requests = []
+        students.forEach(student => requests.push(student.requests.filter(request => request.course_id === req.params.courseID)));
+        res.send({success : true, message: requests})
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+// get all requests
+router.route('/allRequests').get((req, res) => {
+    Student.find()
+    .then(students => {
+        requests = []
+        students.forEach(student => requests.push(student.requests));
+        res.send({success : true, message: requests})
+    })
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+// add request to student requests list
 router.route('/add/request/:id').post((req, res) => {
     Student.findById((req.params.id), (err,student) => {
-        
+        new_request = req.body
         if(err) {
             return res.send({success : false, message:"Error: " + err})
         } else if (!student || student.length === 0) {
             return res.send({success : false, message:"!הסטודנט אינו קיים במערכת" })
-        } else {    
-            if (student.requests.find(request => request.course_id === req.body.course_id)){
-                return res.send({success : false, message:"הקורס כבר קיים בבקשות הסטודנט" })
+        } else {   
+
+            course = student.courses.find(({course_id}) => course_id === new_request.course_id )
+            if (course){
+                gap = Number(student.group.aproved_hours) - Number(course.approved_hours) - Number(course.wating_hours) + Number(course.hours_already_done) 
+                console.log('gap: ' + gap)
+                if (gap < Number(student.group.aproved_hours)){
+                    hours_to_add = Math.min(Number(new_request.number_of_hours), gap)
+                    console.log('hours_to_add: ' + hours_to_add)
+                    final_hours = hours_to_add + Number(course.wating_hours)
+                    console.log('final_hours: ' + final_hours)
+                    new_request.number_of_hours = final_hours.toString()
+                    student.requests = student.requests.filter(request => request.course_id != course.course_id)
+                    changehours(final_hours,course.course_id, student.courses)
+                }
+                else{
+                   return res.send({success : false, message:"!כמות השעות המבוקשת הינה מעל הכמות המותרת עבור הסטודנט" })
+                }
+            // add new course to list of the courses
+            }else{
+                new_course = {course_id: new_request.course_id,
+                            course_name: new_request.course_name,
+                            approved_hours: '0',
+                            wating_hours: new_request.number_of_hours,
+                            hours_already_done: '0'}
+                student.courses.push(new_course)
             }
-            student.requests.push(req.body)
+            new_request.status = 'waiting'
+            new_request.updated_at = Date.now()
+            student.requests.push(new_request)
             student.save((err, doc)=> {
                 if (err) {
                     return res.send({success:false, message:"Error: Couldn't Save " + err})
@@ -62,12 +164,13 @@ router.route('/add/request/:id').post((req, res) => {
 // add student
 router.route('/add').post((req, res) => {
     _id = req.body._id
-    requests = []
-    student_obj = { _id : _id, requests : requests}
-    const {group } = req.body 
-    if (group && group !== '') {
-        student_obj[group] = group
+    group = ''
+    if (req.body.hasOwnProperty('group')){
+        group = req.body.group
     }
+    requests = []
+    courses = []
+    student_obj = { _id : _id, requests : requests, courses : courses, group : group}
     const newStudent = new Student(student_obj)
     newStudent.save((err, student)=> {
         if (err) {
@@ -95,6 +198,7 @@ router.route('/update/:id').post((req,res) => {
         student._id = req.body._id.trim();
         student.group = req.body.group;
         student.requests = req.body.requests;
+        student.courses = req.body.courses;
         student.save((err, doc)=> {
             if(err) {
                 console.log('Error: ' + err);
@@ -106,6 +210,61 @@ router.route('/update/:id').post((req,res) => {
 });
 
 
+// update student's group by id
+router.route('/update/group/:id').post((req,res) => {
+    Student.findById((req.params.id)).then((student) => {
+        if (!student || student.length === 0) {
+            return res.send({success : false, message : "!הסטודנט אינו קיים במערכת"})
+        }
+        student.group = req.body.group;
+        student.save((err, doc)=> {
+            if(err) {
+                console.log('Error: ' + err);
+                return res.send({success : false, message : err.errmsg});
+            }
+            return res.send({success : true, message : "!הקבוצה של הסטודנט עודכנה בהצלחה"});
+        })
+    }); 
+});
+
+
+// update request status by id and course_id
+router.route('/update/requestStatus/:id').post((req,res) => {
+    Student.findById((req.params.id)).then((student) => {
+        if (!student || student.length === 0) {
+            return res.send({success : false, message : "!הסטודנט אינו קיים במערכת"})
+        }
+        new_request = req.body;
+        current_request = student.requests.filter(request => request.course_id === new_request.course_id)
+        if (!current_request || current_request.length===0) {
+            return res.send({success : false, message:"!הבקשה אינה קיימת במערכת" })
+        }
+        else{
+
+            current_request = current_request[0]
+            if (new_request.status === 'approved'){
+                course_to_update = student.courses.findIndex(course => course.course_id === new_request.course_id)
+                current_approved_hours = student.courses[course_to_update].approved_hours
+                updated_hours = Number(current_approved_hours) + Number(current_request.number_of_hours)
+                student.courses[course_to_update].approved_hours = updated_hours.toString()
+                student.courses[course_to_update].wating_hours = '0'
+                student.requests = student.requests.filter(request => request.course_id != current_request.course_id)
+            }else{
+                status_to_update = student.requests.findIndex(requests => requests.course_id === new_request.course_id)
+                student.requests[status_to_update].status = new_request.status
+            }
+            student.save((err, doc)=> {
+                if(err) {
+                    console.log('Error: ' + err);
+                    return res.send({success : false, message : err.errmsg});
+                }
+                return res.send({success : true, message : "!סטטוס הבקשה עודכן בהצלחה"});
+            })
+        }
+    }); 
+});
+
+// delete request by id and course_id
 router.route('/delete/request/:id').post((req, res) => {
     Student.findById((req.params.id), (err,student) => {
         if(err) {
@@ -113,7 +272,7 @@ router.route('/delete/request/:id').post((req, res) => {
         } else if (!student || student.length === 0) {
             return res.send({success : false, message:"!הסטודנט אינו קיים במערכת" })
         } else {
-            student.requests = student.requests.filter(request => request != req.body.request_id)
+            student.requests = student.requests.filter(request => request.course_id != req.body.course_id)
             student.save((err, student)=> {
                 if (err) {
                     return res.send({success:false, message:"Error: Couldn't Save " + err})
@@ -124,5 +283,14 @@ router.route('/delete/request/:id').post((req, res) => {
     })
 })
 
+
+function changehours( new_hours, course_id, courses ) {
+    for (var i in courses) {
+      if (courses[i].course_id == course_id) {
+          courses[i].wating_hours = new_hours;
+         break; //Stop this loop, we found it!
+      }
+    }
+ }
 
 module.exports = router;
