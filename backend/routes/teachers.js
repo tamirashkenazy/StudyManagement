63823aaeb9216315ff8cd6af2bb731e4c2a2aee9
Tamir Ashkenazy <tamir.ash4@gmail.com>
2020-02-28 12:59:16 +0200

    a

--- backend/routes/teachers.js
+++ backend/routes/teachers.js
@@ -21,19 +21,10 @@ router.route('/byID/:id').get((req,res) => {
     Teacher.findById((req.params.id), (err,teacher) => {
         if(err) {
             return res.send({success : false, message:"Error: " + err})
-<<<<<<< HEAD
-<<<<<<< HEAD
-        } else if (!teacher || teacher.length===0) {
-=======
-=======
->>>>>>> admin tables
         } else if (teacher && !Array.isArray(teacher)) {
             return res.send({success : true, message: teacher})
         } else {
->>>>>>> admin tables
             return res.send({success : false, message:"המורה אינו קיים במערכת" })
-        } else {
-            return res.send({success : true, message: teacher})
         }
     })
 })
