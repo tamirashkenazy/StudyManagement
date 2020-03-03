795688655b2ef338a076808411e11313c67b478a
Tamir Ashkenazy <tamir.ash4@gmail.com>
2020-03-03 21:14:39 +0200

    rebasing2

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/App.scss';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {useAsyncHook} from './components/mongo/paths.component'
+
 import LandingPage from './components/login/LandingPage.component';
 import signupConatainer from './components/signup/signupFormContainer.component';
 import Main from './components/main/dashboard_main';
@@ -9,27 +9,19 @@ import Student from './components/main/student/student.component';
 import Teacher from './components/main/teacher/teacher.component';
 import Admin from './components/main/admin/admin.component';
 
-
 function App() {
-  const [teachers, isLoading_teachers] = useAsyncHook(`teachers`)
-  const [students, isLoading_students] = useAsyncHook(`students`)
-  const [users, isLoading_users] = useAsyncHook('users')
   return (
-    (!isLoading_teachers && !isLoading_students && !isLoading_users) ? 
     <Router>
       <Switch>
           <Route path="/signup" component={signupConatainer} />
-          <Route path="/main/student" render={ (props)=> <Student {...props} students={students}/>}/>
-          <Route path="/main/teacher" render={(props)=><Teacher {...props} teachers={teachers} />} />
-          <Route path="/main/admin" render={(props)=><Admin  {...props} teachers={teachers} students={students} users={users}/>}/>
-          {/* //https://tylermcginnis.com/react-router-pass-props-to-components/ */}
-          <Route path="/main" render={(props)=><Main  {...props}  teachers={teachers}/>}/>
+          <Route path="/main/student" component={Student}/>
+          <Route path="/main/teacher" component={Teacher}/>
+          <Route path="/main/admin" component={Admin}/>
+          <Route path="/main" component={Main}/>
           {/**landing page is the sign in page */}
           <Route path="/" exact component={LandingPage}/> 
         </Switch>
     </Router>
-    :
-    <label>Loading</label>
   );
 }
 export default App;
