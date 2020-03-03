0368250afb5900d776983862c06fdb6cfc52343d
Tamir Ashkenazy <tamir.ash4@gmail.com>
2020-03-03 22:47:42 +0200

    rebase22_47

--- client/src/components/mongo/paths.component.js
+++ client/src/components/mongo/paths.component.js
@@ -36,10 +36,7 @@ export function useAsyncHook(api, func_to_sort, args=null) {
             setLoading(false)
         }
         getDataFromAPI()
-    },[api, func_to_sort, args])
-    // if (api === "students") {
-    //     console.log(result);   
-    // }
+    },[])
     return [result, loading];
 }
 
