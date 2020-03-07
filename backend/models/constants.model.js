a70aecb8a81cecff91fc2d358a5085514a11cfc5
michaelreu <michael.reubinof@live.biu.ac.il>
2020-03-07 04:04:58 -0800

    fixed

--- backend/models/constants.model.js
+++ backend/models/constants.model.js
@@ -3,7 +3,7 @@ const onlyNumbers = /^[-0-9\b]+$/;
 
 const Schema = mongoose.Schema;
 const ConstantsSchema = new Schema({
-    uniqe : {type : String, default: "constants", unique: true},
+    unique : {type : String, default: "constants", unique: true},
     lesson_price : { type : String, match: onlyNumbers },
     annual_budget : { type : String, match: onlyNumbers }
 }, { timestamps:true }, );
