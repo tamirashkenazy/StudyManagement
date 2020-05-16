3f8784ce77f2c18edf4902c74893d5c9db442da6
Tamir Ashkenazy <Tamir_Ashkenazy@apple.com>
2020-05-16 10:33:27 +0300

    statistics pie chart ready

--- client/src/components/main/admin/statistics.component.js
+++ client/src/components/main/admin/statistics.component.js
@@ -3,23 +3,23 @@ import {StatisticsPieChart} from '../utils/charts'
 import Grid from '@material-ui/core/Grid';
 
 
-const convert_to_percent = (pie) => {
-    var sum_of_labels = 0
-    for(var i in pie){
-        sum_of_labels += pie[i].sum
-    }
-    for(i in pie){
-        pie[i].sum = (pie[i].sum / sum_of_labels * 100)
-    }
-    return pie;
+const convert_to_percent = (data_points_arr) => {
+    let sum_of_labels = 0
+    data_points_arr.forEach(data_point_obj=>{
+        sum_of_labels += data_point_obj.y
+    })
+    data_points_arr.forEach(pie_obj=>
+        pie_obj.y = (pie_obj.y * 100 / sum_of_labels).toFixed(2)
+    )
+    return data_points_arr;
 }
 
 
 const to_data_points = (arr_to_pie) => {
-    var pie = []
-    for(var i in arr_to_pie){
-        pie.push({label : arr_to_pie[i].name, y: arr_to_pie[i].sum})
-    }
+    let pie = []
+    arr_to_pie.forEach(data_obj => {
+        pie.push({label : data_obj.name, y: data_obj.sum})
+    })
     return pie
 }
 
@@ -28,18 +28,17 @@ export const Statistics = (lessons_pie, students_pie, teachers_pie) => {
     lessons_pie = to_data_points(lessons_pie)
     students_pie = to_data_points(students_pie)
     teachers_pie = to_data_points(teachers_pie)
-
     lessons_pie = convert_to_percent(lessons_pie)
     return (
         <Grid container spacing={2}  direction="column" >
             <Grid item xs >
-                <StatisticsPieChart title={"חלוקת שיעורים"} showPercent={true} data_points={lessons_pie} theme={"light1"} />
+                <StatisticsPieChart title={"חלוקת שיעורים"} label_suffix="%" data_points={lessons_pie} theme={"light1"} />
             </Grid>
             <Grid item xs >
-                <StatisticsPieChart title={"מספר סטודנטים בכל קורס"} showPercent={false} data_points={students_pie} theme={"dark2"} />
+                <StatisticsPieChart title={"מספר סטודנטים בכל קורס"} label_suffix=" סטודנטים " data_points={students_pie} theme={"dark2"} />
             </Grid>
             <Grid item xs >
-                <StatisticsPieChart title={"מספר המורים בכל קורס"} data_points={teachers_pie} theme={"light1"} />
+                <StatisticsPieChart title={"מספר המורים בכל קורס"} label_suffix=" מורים " data_points={teachers_pie} theme={"light1"} />
             </Grid>
         </Grid>
     )
