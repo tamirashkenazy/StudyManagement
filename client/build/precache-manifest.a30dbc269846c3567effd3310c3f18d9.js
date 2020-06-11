6ebbb7ad9a3c8c4c66b482bb029b671955351d98
Tamir Ashkenazy <Tamir_Ashkenazy@apple.com>
2020-06-11 21:50:17 +0300

    build

---
+++ client/build/precache-manifest.a30dbc269846c3567effd3310c3f18d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16213c2413ceff04e451ee3340dccb40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f8c68687bf8806441c8",
+    "url": "/static/css/2.0e8436ce.chunk.css"
+  },
+  {
+    "revision": "3d9410b3b260f6c856df",
+    "url": "/static/css/main.3aae0ddb.chunk.css"
+  },
+  {
+    "revision": "2f8c68687bf8806441c8",
+    "url": "/static/js/2.462b21cc.chunk.js"
+  },
+  {
+    "revision": "e05ce8dcc40a69cef75d91a64b8c05f7",
+    "url": "/static/js/2.462b21cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d9410b3b260f6c856df",
+    "url": "/static/js/main.124599e7.chunk.js"
+  },
+  {
+    "revision": "779b251dde83a6779b96",
+    "url": "/static/js/runtime-main.4c144e55.js"
+  },
+  {
+    "revision": "33a4d2d9e165223d77b0568ddf40c7ba",
+    "url": "/static/media/login_bg.33a4d2d9.jpg"
+  },
+  {
+    "revision": "cf77b06bb143a20aa23f739c08d5fd0a",
+    "url": "/static/media/login_people_books.cf77b06b.jpg"
+  },
+  {
+    "revision": "fa7cca0da9fc674c4e3e934f04fcb864",
+    "url": "/static/media/study_management.fa7cca0d.png"
+  }
+]);
